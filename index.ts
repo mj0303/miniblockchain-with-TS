4b03c12bbbe6e3b7d5dabfdea984cd70604cb3ed
mj0303 <61722848+mj0303@users.noreply.github.com>
2021-12-14 12:59:27 +0900

    Setting ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "MJ0303",
+age = 23,
+gender = "male"
+
+const sayHi = (name, age, gender) => {
+   console.log(`Hello this is ${name}. I'm ${age} and ${gender}.`);
+}
+
+sayHi(name, age, gender);
+
+export {};
